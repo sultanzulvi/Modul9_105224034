42502128a0e02aab4b302d3996af873c3d9df621
sultanzulvi <akundump27@gmail.com>
2026-05-19 14:34:39 +0700

    pretetst

--- PreTest/PreTest/src/KomputerServer.java
+++ PreTest/PreTest/src/KomputerServer.java
@@ -1,3 +1,10 @@
 public class KomputerServer {
+    protected HardDisk HD;
+    protected Monitor monitor;
 
+    public KomputerServer(Monitor monitor){
+        this.HD = new HardDisk();
+        this.monitor = monitor;
+    }
+    
 }
